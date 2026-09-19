1b79199e5161f866dcc60ef2e6ea92f6806ae62a
Pablo Toapanta <toapantapablo233@gmail.com>
2026-09-19 10:54:56 -0500

    feat(assets): agrega infraestructura de datos (modelos Activo/Categoria, DbContext, connection string)

--- backend/AssetsService/Program.cs
+++ backend/AssetsService/Program.cs
@@ -1,3 +1,6 @@
+using AssetsService.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Registra el DbContext con la connection string de AssetsDB (usuario assets_user).
+builder.Services.AddDbContext<AssetsDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AssetsDB")));
 
 var app = builder.Build();
 
