b9df775d8843df8b34f2196ba40d60d42e95af24
Pablo Toapanta <toapantapablo233@gmail.com>
2026-09-19 10:56:56 -0500

    feat(assets): agrega ActivosController (POST, GET lista, GET depreciacion) con Swagger

--- backend/AssetsService/Program.cs
+++ backend/AssetsService/Program.cs
@@ -1,3 +1,4 @@
+using System.Reflection;
 using AssetsService.Data;
 using AssetsService.Services;
 using Microsoft.EntityFrameworkCore;
@@ -9,7 +10,13 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    // Lee los comentarios XML (/// summary) para documentar los endpoints en Swagger
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    options.IncludeXmlComments(xmlPath);
+});
 
 // Registra el DbContext con la connection string de AssetsDB (usuario assets_user).
 builder.Services.AddDbContext<AssetsDbContext>(options =>
